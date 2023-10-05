64fde75c1663605b1ae56681f87106e076c49bdf
dorotheruys <dorothe@beruys.be>
2023-10-05 11:13:57 +0200

    added empty function for plotting the velocity triangles for now

--- compressor_design.py
+++ compressor_design.py
@@ -1,4 +1,8 @@
 import numpy as np
+
+def plot_velocity_triangles():
+
+    return
 def compressor_design(eta_comp, P0_in, T0_in, rot_speed, pressure_ratio, mass_flow, R, work_coef, flow_coef):
     # Determine total outlet conditions T0_out and P0_out
 
