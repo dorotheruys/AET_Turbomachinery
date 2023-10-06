5e174deca073b135e2cf11f921b731f6d7ad6bdb
dorotheruys <dorothe@beruys.be>
2023-10-06 15:29:20 +0200

    calculations for blade height

--- compressor_design.py
+++ compressor_design.py
@@ -1,11 +1,13 @@
 import numpy as np
 from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+from Flightengineperformance import total_conditions, station_3, T_a, P_a, M, m_air
 
-R_constant = 287 #J/K*mol
-k_a = 1.4 #-
-k_g = 1.33 #-
-cp_a = 1000 #J/kg K
-cp_g = 1150 #J/kg K
+R_constant = 287        #J/K*mol
+k_a = 1.4               #-
+k_g = 1.33              #-
+cp_a = 1000             #J/kg K
+cp_g = 1150             #J/kg K
 def flow_angle_equations(vars, work_coef, flow_coef, R):
     alpha1, beta2 = vars
     eq1 = 1 - flow_coef * (-np.tan(beta2) + np.tan(alpha1)) - work_coef
@@ -30,7 +32,24 @@ def determine_velocity_triangles(flow_coef, U, alpha1, beta2):
     W_1 = V_m / np.cos(beta1)           # cos(beta1) = V_m/W_1
 
     return V_1, W_1, beta1, V_2, W_2, alpha2, V_m
-def plot_velocity_triangles(V_1, W_1, alpha1, beta1, V_2, W_2, alpha2, beta2):
+def plot_velocity_triangles(V_1, W_1, alpha1, beta1, V_2, W_2, alpha2, beta2, U):
+    angs = [alpha1, beta1, alpha2, beta2, np.pi, np.pi]
+    velos = [V_1, W_1, V_2, W_2, U, U]
+    for i in range(len(angs)):
+        dx = velos[i] * np.sin(angs[i])
+        dy = velos[i] * np.cos(angs[i])
+        if i < 4:
+            plt.arrow(0, 0, dx, dy)
+        elif i == 4:
+            x0 = velos[1] * np.sin(angs[1])
+            y0 = velos[1] * np.cos(angs[1])
+            plt.arrow(x0, y0, dx, dy)
+        elif i == 4:
+            x0 = velos[3] * np.sin(angs[3])
+            y0 = velos[3] * np.cos(angs[3])
+            plt.arrow(x0, y0, dx, dy)
+
+    plt.show()
     return
 
 def calculate_number_of_stages(specific_work, work_coef, target_U):
@@ -84,11 +103,11 @@ def calculate_blade_height(area, r_in):
     blade_height = np.sqrt(area / np.pi + r_in**2) - r_in
     return blade_height
 
-def compressor_design(eta_comp, P0_in, T0_in, rot_speed, pressure_ratio, mass_flow, R, work_coef, flow_coef):
+def compressor_design(eta_comp, Pt_in, Tt_in, Tt_out, mass_flow, R, work_coef, flow_coef, r_in):
     # Determine total outlet conditions T0_out and P0_out
 
     #specific work
-    specific_work = cp_a * (T0_out - T0_in)
+    specific_work = cp_a * (Tt_out - Tt_in)
 
     # Determine number of stages and peripheral speed U with limit of 600 m/s
     nr_stages, U = calculate_number_of_stages(specific_work, work_coef, 600.)
@@ -97,8 +116,9 @@ def compressor_design(eta_comp, P0_in, T0_in, rot_speed, pressure_ratio, mass_fl
     alpha1, beta2 = find_flow_angles_alpha1_beta2(work_coef, flow_coef, R)  # output in radians
     V_1, W_1, beta1, V_2, W_2, alpha2, V_m = determine_velocity_triangles(flow_coef, U, alpha1, beta2)
 
-    Tt_before = Tt_2
-    Pt_before = Pt_2
+    # Run through each stage and determine the thermodynamic properties
+    Tt_before = Tt_in
+    Pt_before = Pt_in
 
     temp_array = np.zeros(2*nr_stages)
     pressure_array = np.zeros(2*nr_stages)
@@ -106,29 +126,34 @@ def compressor_design(eta_comp, P0_in, T0_in, rot_speed, pressure_ratio, mass_fl
     bladeheight_array = np.zeros(2*nr_stages)
 
     for stagenr in range(1, nr_stages+1):
-        Tt_during, Pt_during, Tt_after, Pt_after, area_rotor, area_stator = calculate_thermodynamic_properties_stage(C_p, eff, k, Tt_before, Pt_before, mass_flow, V_m, V_2, W_1, W_2)
+        Tt_during, Pt_during, Tt_after, Pt_after, area_rotor, area_stator = calculate_thermodynamic_properties_stage(cp_a, eta_comp, k_a, Tt_before, Pt_before, mass_flow, V_m, V_2, W_1, W_2)
 
         bladeheight_rotor = calculate_blade_height(area_rotor, r_in)
         bladeheight_stator = calculate_blade_height(area_stator, r_in)
 
-        stagenr_array[stagenr-1] = stagenr
+        stagenr_array[2*(stagenr-1)] = stagenr
         temp_array[2*(stagenr-1)] = Tt_during
         temp_array[2*(stagenr-1)+1] = Tt_after
         pressure_array[2*(stagenr-1)] = Pt_during
         pressure_array[2*(stagenr-1)+1] = Pt_after
-        bladeheight_array[stagenr-1] = [bladeheight_rotor, bladeheight_stator]
+        bladeheight_array[2*(stagenr-1)] = bladeheight_rotor
+        bladeheight_array[2*(stagenr-1)+1] = bladeheight_stator
 
         Tt_before = Tt_after
         Pt_before = Tt_after
+
+
     return
 
 
 # # Example usage:
-# work_coef = 0.38
-# flow_coef = 0.77
-# R = 0.1
-#
-# alpha1, beta2 = find_flow_angles_alpha1_beta2(work_coef, flow_coef, R)
-# print(f"Alpha1: {alpha1}, Beta2: {beta2}")
+work_coef = 0.38
+flow_coef = 0.77
+R = 0.5
+eta_comp = 1.
+r_in = 0.5
 
-compressor_design()
+Pt_in, Tt_in = total_conditions(P_a, T_a, k_a, M)
+Pt_out, Tt_out, W_comp = station_3(Pt_in, Tt_in ,k_a ,cp_a, m_air, eta_comp)
+
+compressor_design(eta_comp, Pt_in, Tt_in, Tt_out, m_air, R, work_coef, flow_coef, r_in)
